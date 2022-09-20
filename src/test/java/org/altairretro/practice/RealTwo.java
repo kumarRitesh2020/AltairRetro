43e8a266d55dc407b8cf61f5999aa450945f7d1c
User <User@DESKTOP-TTAV2T1>
2022-09-20 17:02:49 +0530

    first project commit

---
+++ src/test/java/org/altairretro/practice/RealTwo.java
@@ -0,0 +1,18 @@
+package org.altairretro.practice;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+public class RealTwo {
+	
+	
+	@Test
+	public void Delhi() {
+		System.out.println("this will be executed after every test cases");
+	}
+	@Test
+	public void test2() {
+		System.out.println("test2 of real 2 executed");
+	}
+
+}
